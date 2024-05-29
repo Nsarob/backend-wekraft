a5d825def540f28842bdfaf1a0801d287af0480e
NIYONIZEYE Faustin <142995193+niyonizeyefaustin@users.noreply.github.com>
2024-05-29 21:41:46 +0200

    Update velifyaccess.js

--- src/middlewares/velifyaccess.js
+++ src/middlewares/velifyaccess.js
@@ -10,7 +10,7 @@ const VerifyAccess = (passRole)=>{
       return errormessage(res,401,`no token provided`)
     }else{
      try {
-      const verifyToken = jwt.verify(token,process.env.SCRET_KEY,{expiresIn:"2d"})
+      const verifyToken = jwt.verify(token,process.env.SECRET_KEY,{expiresIn:"2d"})
       req.user=verifyToken.user
       if(passRole !== verifyToken.user.role){
         return errormessage(res,401,`please you have not access`)
