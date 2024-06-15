8ae95d82f10df8aa3928ed04d6cbf89c2a5ec669
nsarob <roboticsdamour447@gmail.com>
2024-06-15 23:40:47 +0200

    Testing .env variables fetch

--- src/utiles/cloud.js
+++ src/utiles/cloud.js
@@ -14,4 +14,10 @@ cloudinary.config({
   api_secret: process.env.cloudinary_api_secret
 });
 
+console.log("CLoudinary: ",{ 
+  cloud_name: process.env.cloud_name, 
+  api_key:  process.env.cloudinary_api_key, 
+  api_secret: process.env.cloudinary_api_secret
+})
+
 export default cloudinary
